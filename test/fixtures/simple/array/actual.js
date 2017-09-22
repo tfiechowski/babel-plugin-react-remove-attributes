806263e032ffafa72c0783c6ce8c1bbebd7e4e60
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2017-09-22 14:12:19 +0200

    Add one layer to fixture structure

---
+++ test/fixtures/simple/array/actual.js
@@ -0,0 +1,5 @@
+const arr = ["1", "2", "3"];
+
+const e = (
+  <div>{arr.map(el => <span key={el} data-test-id={`span-key-${el}`} />)}</div>
+);
---
+++ test/fixtures/simple/array/expected.js
@@ -0,0 +1,3 @@
+const arr = ["1", "2", "3"];
+
+const e = <div>{arr.map(el => <span key={el} />)}</div>;
