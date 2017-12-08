914010a6c294b05cb14e563391cbd98c67f225eb
fatfisz <fatfisz@gmail.com>
2017-12-09 00:09:33 +0100

    Add support for regular expressions

--- src/jsx-attribute-visitor.js
+++ src/jsx-attribute-visitor.js
@@ -1,3 +1,13 @@
+function matchesId(name, id) {
+  if (typeof id === 'string') {
+    return name === id;
+  }
+
+  // Let's be lazy and assume RegExp
+
+  return id.test(name);
+}
+
 export default (path, state) => {
   const { types } = state;
   const { ids } = state.opts;
@@ -9,7 +19,10 @@ export default (path, state) => {
   const name = path.get('name');
   const actualName = name.node.name;
 
-  if (ids.includes(actualName)) {
-    path.remove();
+  for (const id of ids) {
+    if (matchesId(actualName, id)) {
+      path.remove();
+      return;
+    }
   }
 };
