0115aa0d31fb32582b4655cce8398968b87ae38f
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2017-12-11 21:21:01 +0100

    Add support for regex queries

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import babelPluginSyntaxJSX from 'babel-plugin-syntax-jsx';
 import attributeVisitor from './jsx-attribute-visitor';
+import { isEqual } from 'lodash';
 
 export default function() {
   return {
     pre() {
-      if (!Array.isArray(this.opts)) {
+      if (!this.opts || isEqual(this.opts, {})) {
         this.opts = ['data-test-id'];
+      } else {
+        this.opts = Array.isArray(this.opts) ? this.opts : [this.opts];
       }
     },
     inherits: babelPluginSyntaxJSX,
--- src/jsx-attribute-visitor.js
+++ src/jsx-attribute-visitor.js
@@ -1,7 +1,12 @@
 export default (path, state) => {
-  const attributes = state.opts;
+  const attributes = state.opts || [];
 
-  if (attributes.includes(path.node.name)) {
-    path.parentPath.remove();
-  }
+  attributes.forEach(attribute => {
+    const removeStringOpt = typeof attribute === 'string' && attribute === path.node.name;
+    const removeRegexpOpt = attribute instanceof RegExp && path.node.name.match(attribute);
+
+    if (removeStringOpt || removeRegexpOpt) {
+      path.parentPath.remove();
+    }
+  });
 };
